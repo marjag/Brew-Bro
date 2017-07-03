246ac4e6f915393e21f3f89710e128bd89f0677c
Frosty8 <frostydev8@gmail.com>
2017-07-03 15:20:35 +0200

    Stworzono klasę BrewBro obliczającą parametry brzeczki, stworzono klasę encji Post, stworzono podstawowe kontrolery i widoki (kilku jeszcze brakuje), opracowano logo

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -5,17 +5,18 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response as Response;
+
 
 class HomeController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request) : Response
     {
-        // replace this example code with whatever you need
         return $this->render('home/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+
         ]);
     }
 }
